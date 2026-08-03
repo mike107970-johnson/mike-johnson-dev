22a8d3f03c935ee236caa38791d89bb346f62385
Mike Johnson <mike107970@gmail.com>
2026-08-03 13:00:45 -0300

    Build Android APK with installed Gradle

---
+++ android/app/src/androidTest/java/com/footballvows/app/ExampleInstrumentedTest.java
@@ -0,0 +1,12 @@
+package com.footballvows.app;
+
+import static org.junit.Assert.assertEquals;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test public void packageNameIsFootballVows() { assertEquals("com.footballvows.app", InstrumentationRegistry.getInstrumentation().getTargetContext().getPackageName()); }
+}
